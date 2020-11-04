069915da6b5c3a4c5112323584e8f5be92b08208
Mimi-12 <oneiconforlife26@gmail.com>
2020-11-03 23:22:36 -0800

    commit tree f8753e693853ff2f31ff36799b2e66db7fc5bf30

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") 
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") 
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") 
+      true
+    else
+      false
+    end
   end
 
-  def count_sentences
+  
+     #self.split("").find_all{|l| l == ". " || l == "? "  || l == "! "}.count
+     def count_sentences
+      sentence = 0
+      array = self.split
+      array.each do |phrase|
+      sentence += 1 if phrase.sentence? || phrase.exclamation? || phrase.question?
+      end
+      sentence
+      
+      end
+    
+  
 
-  end
 end
